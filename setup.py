bee84c826b729fee872fdf6b49c553e99bb15cb3
ubhadra <bhadra.upahar@gmail.com>
2024-10-01 21:54:36 +0530

    Code updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="defectTracer",
+    version="0.0.1",
+    author="Upahar Bhadra",
+    author_email="bhadra.upahar@gmail.com",
+    packages=find_packages()
+)
